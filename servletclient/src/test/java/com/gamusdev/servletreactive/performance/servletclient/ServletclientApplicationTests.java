7db095ab2335b2d0a5e3bb69f750e9ce72b45beb
DRamirez <dramirez@gamusdev.com>
2022-10-06 01:06:16 +0200

    Init servletclient

---
+++ servletclient/src/test/java/com/gamusdev/servletreactive/performance/servletclient/ServletclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamusdev.servletreactive.performance.servletclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
